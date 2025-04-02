bc52525602cefa3bc222bad6048311758e774715
Denise Wagenführ <denise.wagenfuehr@capgemini.com>
2025-04-03 00:46:29 +0200

    remove parts

--- src/components/Mealplan.jsx
+++ src/components/Mealplan.jsx
@@ -7,15 +7,10 @@ export default function Mealplan({ week }) {
       <Table.Tbody>
         {week.meals.map((meal) => (
             <>
-          <Table.Tr key={meal.weekday}>
-            <Table.Td colSpan={3}><Card><Title order={2}>{meal.weekday}</Title></Card></Table.Td>
-            </Table.Tr>
-            <Table.Tr key={`${meal.weekday}-meals`}>
-            <Table.Td><MealCard meal={meal.pre} cardTitle={"Vorspeise"}/></Table.Td>
-            <Table.Td><MealCard meal={meal.main} cardTitle={"Hauptspeise"} style={{height: '100%'}}/></Table.Td>
-            <Table.Td><MealCard meal={meal.dessert} cardTitle={"Dessert"}/></Table.Td>
-          </Table.Tr>
-          </>
+              <Table.Tr key={`${meal.weekday}-meals`}>
+                <Table.Td><MealCard meal={meal.pre} cardTitle={"Vorspeise"}/></Table.Td>
+              </Table.Tr>
+            </>
         ))}
       </Table.Tbody>
     </Table>
